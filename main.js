0899f67088f96286609d697e294e17fe3ed604f9
ibrahemyasser <64380956+ibrahemyasser@users.noreply.github.com>
2021-06-09 20:23:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,80 @@
-const doc = document;
-const menuOpen = doc.querySelector(".menu");
-const menuClose = doc.querySelector(".close");
-const overlay = doc.querySelector(".overlay");
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+var count = 0;
 
-menuOpen.addEventListener("click", () => {
-  overlay.classList.add("overlay--active");
-});
-
-menuClose.addEventListener("click", () => {
-  overlay.classList.remove("overlay--active");
-});
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 1000); 
+//Show input error message
+function ShowError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "input-box-error";
+  const small = formControl.querySelector("small");
+	small.innerText = message;
+	count++;
 }
 
-const form = document.getElementById('form');
-const password = document.getElementById('password');
-const error = document.getElementById('error');
+//Show input success
+function ShowSuccess(input) {
+  const formControl = input.parentElement;
+	formControl.className = "input-box-success";
+}
 
-///const regex = /\d/;
-//const doesItHaveNumber = regex.test(pass.value);
+function CheckRequired(inputErr) {
+  inputErr.forEach(function (input) {
+	if (input.value.trim() === "") {
+	  ShowError(input, `${getFieldName(input)} is required`);
+	} else {
+	  ShowSuccess(input);
+	}
+  });
+}
+
+function CheckPassword(input) {
+  if (password.value.length !== 8) {
+	ShowError(input, `${getFieldName(input)} must be 8 characters`);
+  }
+  if (/[a-zA-Z]/.test(password.value.substring(0, 1)) != true) {
+	ShowError(input, `${getFieldName(input)}  must start letter`);
+  }
+  if (
+	password.value.substring(0, 1) !==
+	password.value.substring(0, 1).toUpperCase()
+  ) {
+	ShowError(
+	  input,
+	  `${getFieldName(input)}  must start with Upper case letter`
+	);
+  }
+  if (/\d/.test(password.value) != true) {
+	ShowError(
+	  input,
+	  `${getFieldName(input)}  must include at least one number`
+	);
+  }
+  if (
+	/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password.value) !=
+	true
+  ) {
+	ShowError(
+	  input,
+	  `${getFieldName(
+		input
+	  )}  must include at least one special charactar`
+	);
+  }
+  if (/\s/.test(password.value) == true) {
+	ShowError(input, `${getFieldName(input)}  can not contain spaces`);
+  }
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 form.addEventListener('submit', (e) => {
-
-    let meessages = [];
-    if (password.value.length !== 8) {
-        meessages.push("password must be 8 charactars")
-    }
-    if (((/[a-zA-Z]/.test(password.value.substring(0, 1))) != true)) {
-        meessages.push("password must start with letter");
-    }
-    if (password.value.substring(0, 1) !== (password.value.substring(0, 1)).toUpperCase()) {
-        meessages.push("First letter must be Upper case");
-    }
-    if ((/\d/.test(password.value) != true)) {
-        meessages.push("password must include at least one number");
-    }
-    if ((/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password.value) != true)) {
-        meessages.push("password must include at least one special charactar");
-    }
-    if (((/\s/.test(password.value)) == true)) {
-        meessages.push("password mus't include spaces");
-    }
-    if (meessages.length > 0) {
-        e.preventDefault()
-        error.innerText = meessages.join(" _ ")
-    }
-})
+count = 0;
+  CheckRequired([password]);
+  CheckPassword(password);
+  	if (count !== 0) {
+		e.preventDefault();
+	}
+});
